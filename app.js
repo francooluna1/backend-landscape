9bbd621e121463d838fc7a45275e3a5cfab4ee8b
Franco Luna <fluna@siltium.com>
2023-10-12 11:52:18 -0300

    Added app.use to avoid CORS error

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+});
+
 app.get("/getActivities", (req, res) => {
   const activities = [
     {
